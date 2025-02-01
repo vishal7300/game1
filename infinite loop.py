6c0dffcd8febedef01f7c9d9f3202c7f401e0906
vishal7300 <shobu85sai@gmail.com>
2025-02-01 16:55:47 +0530

    Create infinite loop.py

---
+++ infinite loop.py
@@ -0,0 +1,8 @@
+while True:
+    speed=int(input("enter your max speed in car:"))
+    if speed<250:
+        print("you are a noob ",speed)
+
+    if speed > 250:
+        print("you are pro")
+        break
